b29bfd2ce3193db9b6da52bffd6cece24126d59b
ktofel3 <kctofel@gatech.edu>
2022-02-03 14:48:15 -0500

    Finished calculator

--- calculator.js
+++ calculator.js
@@ -14,4 +14,21 @@ let number1 = readline.question(); // Ask the user for the first number.
 console.log(`What's the second number?`);
 let number2 = readline.question(); // Ask the user for the second number.
 
-console.log(`${number1} ${number2}`);
+// console.log(`${number1} ${number2}`); log for test purposes only
+
+console.log(`What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
+let operation = readline.question(); // Ask the user for an operation to perform.
+
+let output;
+
+if (operation === '1') { // Perform the operation on the two numbers.
+  output = Number(number1) + Number(number2); // convert string inputs to number data type
+} else if (operation === '2') {
+  output = Number(number1) - Number(number2);
+} else if (operation === '3') {
+  output = Number(number1) * Number(number2);
+} else if (operation === '4') {
+  output = Number(number1) / Number(number2);
+}
+
+console.log(`The results is: ${output}`); // Print the result to the terminal.
