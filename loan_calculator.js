5ba61669e2b3061349142c37c3fd086afb07f193
Kevin C. Tofel <kevin@kctofel.com>
2022-03-19 17:33:07 -0400

    First input

--- loan_calculator.js
+++ loan_calculator.js
@@ -4,7 +4,27 @@
 // outputs:
 //    monthly interest rate, loan duration in months
 
+const prompts = require('./modules/loan_prompts.json'); // input prompts in JSON
+const prompt = (message) => ( // refactored function for nicer prompt
+  console.log(`=> ${message}`));
+
 let loanAmount, monthlyRate, loanDuration;
 
 // eslint-disable-next-line max-len
 let payment = loanAmount * (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanDuration))));
+const readline = require('readline-sync'); // needed for input
+
+// Get loan amount
+function getAmount() {
+  console.clear();
+  prompt(prompts.welcome);
+  prompt(prompts.amount);
+  loanAmount = Number(readline.question()).toFixed(2); // Note: STRING
+  console.log(`$${loanAmount}`);
+}
+
+// Calculate monthlyRate and loan duration in months
+
+// Display outputs
+
+getAmount();
